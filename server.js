bf1813edcfe79cc9e66469afc12ae461095f7b7d
NeverMinD <46562212+xenowing@users.noreply.github.com>
2019-09-09 22:18:17 +0545

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+const http = require('http');
+const serv = http.Server(app)
+const io = require('socket.io')(serv, {});
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
+app.use(express.static('public'));
+
+
+app.get('/', function(request, response) {
+  console.log(`Pinged ${Date()}`)
+  response.sendFile(__dirname + '/views/index.html');
+});
+app.use('/', express.static(__dirname + '/views'));
+
+
+let locx, locy
+io.on('connection', async function(socket){
+console.log(socket)
+  socket.on('question', async function(data) {
+            let k = await cathyjs.startChatting(data.q)
+              
+                socket.emit('res', {res: k});
+            });
+  
+  })
+
+
+
+var cathyjs = require("cathyjs");
+
+serv.listen(3000, function(){
+  console.log('listening on *:3000');
+});
